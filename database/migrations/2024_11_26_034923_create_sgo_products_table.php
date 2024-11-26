c81da8821bfeca5bea63b6c2445f28dca4219184
dev_phongpt <phamtuanphongisme@gmail.com>
2024-11-26 14:11:50 +0700

    add models

---
+++ database/migrations/2024_11_26_034923_create_sgo_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sgo_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->longText('images')->nullable();
+            $table->string('source')->nullable();
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->foreign('company_id')->references('id')->on('sgo_companies')->onDelete('cascade');
+            $table->unsignedBigInteger('country_id')->nullable();
+            $table->foreign('country_id')->references('id')->on('sgo_countries')->onDelete('cascade');
+            $table->integer('condition_level')->nullable();
+            $table->integer('guarantee')->nullable();
+            $table->bigInteger('price')->nullable();
+            $table->integer('status');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sgo_products');
+    }
+};
